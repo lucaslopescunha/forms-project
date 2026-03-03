c8749087bdfa0cb3805cedf24a362dfb51a189b4
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-03 15:47:27 -0300

    Required fields.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,7 +9,17 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   onSubmit(form: NgForm) {
+    if(form.form.invalid) {
+      return;
+    }
+
+    // const enteredEmail = form.form.value.email; this is the alternative to the two binding
+    const enteredEmail = form.form.value.email;
+    const enteredPasswrod = form.form.value.password;
     console.log(form);
     console.log(form.form.value);// get the values
+    console.log("email: ", enteredEmail);
+    console.log("enteredPassword: ", enteredPasswrod);
+    form.reset();
   }
 }
