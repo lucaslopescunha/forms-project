7c81e7312d944bbfa4bb6896ceb2ac014fc567cf
lucaslopescunha <lucaslopescunha@gmail.com>
2026-03-03 14:48:48 -0300

    Forms module.

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(form.form.value);// get the values
+  }
+}
